08473c9c59db8eab1bd86173329290cb69e7246d
Edith-Heidmann <eheidmann@gmail.com>
2022-06-21 14:46:19 +0200

    Updated mobile nav styles

--- Components/Navbar/MobileNavLinks.js
+++ Components/Navbar/MobileNavLinks.js
@@ -3,77 +3,69 @@ import { Fragment } from "react";
 import { useAppContext } from "../../Context/Context";
 import { motion } from "framer-motion";
 
-import styles from "./navbar.module.scss"
+import styles from "./navbar.module.scss";
 
 const MobileNavLinks = () => {
   const animateFrom = { opacity: 0, y: -40 };
   const animateTo = { opacity: 1, y: 0 };
 
-
   return (
-<Fragment>
-
-    <motion.ul
-      className={styles["mobile-nav-links-container"]}
-      initial={animateFrom}
-      animate={animateTo}
-      transition={{ delay: 0.05 }}
-    
-    >
-      <motion.li
-        className={styles["nav-links"]}
+    <Fragment>
+      <motion.ul
+        className={styles["mobile-nav-links-container"]}
         initial={animateFrom}
         animate={animateTo}
-        transition={{ delay: 0.2 }}
-        onClick={useAppContext().closeHamburgerDropdownOnClick}
-        
+        transition={{ delay: 0.05 }}
       >
-        <Link href="/portfolio" alt="Link to projects page">
-          Portfolio
-        </Link>
-      </motion.li>
-
-      <motion.li
-      className={styles["nav-links"]}
-        initial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.3 }}
-        onClick={useAppContext().closeHamburgerDropdownOnClick}
-      >
-        <Link href="/webapps" alt="Link to Web Apps page">
-          Web-Apps
-        </Link>
-      </motion.li>
-
-      <motion.li
-      className={styles["nav-links"]}
-        initial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.4 }}
-        onClick={useAppContext().closeHamburgerDropdownOnClick}
-      >
-        <Link href="/contact" alt="Link to contact page">
-          Contact
-        </Link>
-      </motion.li>
-
-{/* // Todo: Check that id is Hashlinked to top of landing page? */}
-      <motion.li
-      className={styles["nav-links"]}
-        initial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.5 }}
-        onClick={useAppContext().closeHamburgerDropdownOnClick}
-      >
-        <Link
-          id="nav-home-link"
-          href="/"
-          alt="Link to home/about page"
+        <motion.li
+          className={styles["nav-links"]}
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.2 }}
+          onClick={useAppContext().closeHamburgerDropdownOnClick}
         >
-          Home/About
-        </Link>
-      </motion.li>
-    </motion.ul>
+          <Link href="/portfolio" alt="Link to projects page">
+            Portfolio
+          </Link>
+        </motion.li>
+
+        <motion.li
+          className={styles["nav-links"]}
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.3 }}
+          onClick={useAppContext().closeHamburgerDropdownOnClick}
+        >
+          <Link href="/webapps" alt="Link to Web Apps page">
+            Web-Apps
+          </Link>
+        </motion.li>
+
+        <motion.li
+          className={styles["nav-links"]}
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.4 }}
+          onClick={useAppContext().closeHamburgerDropdownOnClick}
+        >
+          <Link href="/contact" alt="Link to contact page">
+            Contact
+          </Link>
+        </motion.li>
+
+        {/* // Todo: Check that id is Hashlinked to top of landing page? */}
+        <motion.li
+          className={styles["nav-links"]}
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.5 }}
+          onClick={useAppContext().closeHamburgerDropdownOnClick}
+        >
+          <Link id="nav-home-link" href="/" alt="Link to home/about page">
+            Home/About
+          </Link>
+        </motion.li>
+      </motion.ul>
     </Fragment>
   );
 };
