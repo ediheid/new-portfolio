bb81e952efea6c313e5387d9bfcf1cbad8547ca1
Edith-Heidmann <eheidmann@gmail.com>
2022-07-15 14:08:25 +0200

    Added window on scroll and load to useEffect in AppWrapper

--- Context/Context.js
+++ Context/Context.js
@@ -1,19 +1,36 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
 // App Wrapper - (wrapped around App Component)
 export const AppWrapper = ({ children }) => {
-
-  // Hamburger dropdwon state
+  // Hamburger dropdown state
   let [isNavOpen, setIsNavOpen] = useState(false);
-// Functio to close mobile nav when clicking on link
+  //  Function to close mobile nav when clicking on link
   const closeHamburgerDropdownOnClick = () => {
-    setIsNavOpen(false)
-  }
+    setIsNavOpen(false);
+  };
+
+  //  Global Window events
+  useEffect(() => {
+    // Closes mobile nav dropdown if it is open and the user scrolls down the page past 100px
+    window.onscroll = function () {
+      if (window.scrollY > 100) {
+        setIsNavOpen(false);
+      }
+    };
+
+    // Scroll to top on browser refresh
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+    // (Page needs to be rendered before accessing window)
+  }, []);
 
   return (
-    <AppContext.Provider value={{ isNavOpen, setIsNavOpen, closeHamburgerDropdownOnClick }}>
+    <AppContext.Provider
+      value={{ isNavOpen, setIsNavOpen, closeHamburgerDropdownOnClick }}
+    >
       {children}
     </AppContext.Provider>
   );
@@ -25,4 +42,3 @@ export const AppWrapper = ({ children }) => {
 export const useAppContext = () => {
   return useContext(AppContext);
 };
-
