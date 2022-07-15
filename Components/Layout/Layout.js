487c51644eb41779f588d82514dc506fd7568247
Edith-Heidmann <eheidmann@gmail.com>
2022-07-15 15:09:46 +0200

    Moved Head to Layout.js

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -1,9 +1,20 @@
+import Head from "next/head";
 import NavbarContainer from "../Navbar/NavBarContainer";
 import Footer from "../Footer";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Edith Heidmann | Web developer</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="Creative fullstack web developer based in Berlin, specialising in frontend development and UI/UX design."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <NavbarContainer />
       {children}
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,6 @@ import styles from "../pages/landing-page.module.scss";
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Edith Heidmann | Web developer</title>
-        <meta
-          name="description"
-          content="Creative fullstack web developer based in Berlin, specialising in frontend development and UI/UX design."
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <div className={styles["page-container"]}>
         <h1 className={styles["test"]}>This will be the Home page</h1>
       </div>
